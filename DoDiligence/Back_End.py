ab5c5aa534d4c2310bdb1bc03a083aaaac2653a4
SamMorris00 <samorris811@gmail.com>
2022-05-17 22:34:27 +0100

    Back_End repaired

--- DoDiligence/Back_End.py
+++ DoDiligence/Back_End.py
@@ -7,26 +7,25 @@ import pandas as pd
 company_ticker = "LGEN.L"
 whole_company_data = yf.Ticker(company_ticker)
 
-historical_data = whole_company_data.history(start="2000-01-01")
-historical_dividend_data = historical_data['Dividends']
-historical_dividend_payout_dates = historical_dividend_data[historical_dividend_data.Dividends != 0]  # filters old to remove entries = 0
+hist_data = whole_company_data.history(start="2000-01-01")
+hist_div_payout_dates = hist_data[hist_data.Dividends != 0]  # filters old to remove entries = 0
+hist_div_data = hist_div_payout_dates['Dividends']
 
-grouped_historical_dividend_payout_dates = historical_dividend_payout_dates.resample('A').sum()
-print(grouped_historical_dividend_payout_dates)
+grp_hist_div_payout_dates = hist_div_data.resample('A').sum()
+print(grp_hist_div_payout_dates)
 
 # #subplot creation
 fig, (close, dividends) = plt.subplots(nrows=2, ncols=1)
 
-# CLose price plot
-
-y = historical_data['Close']
-x = historical_data.index
+# Historical cLose price plot
+y = hist_data['Close']
+x = hist_data.index
 close.plot(x, y, linewidth=1.0)
 
 # Dividend plot
-y_div = grouped_historical_dividend_payout_dates['Dividends']
-x_div = grouped_historical_dividend_payout_dates.index
-dividends.plot(x_div, y_div, linewidth=1.0)
+y_div = grp_hist_div_payout_dates.iloc[:, ]
+x_div = grp_hist_div_payout_dates.index
+dividends.bar(x_div, y_div,100)
 plt.show()
 
 
