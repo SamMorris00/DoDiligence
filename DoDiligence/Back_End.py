ce691ec5aff0d87622c565c5557899742fdb43b3
SamMorris00 <samorris811@gmail.com>
2022-05-18 21:57:23 +0100

    Dividend yield added into Back_End

--- DoDiligence/Back_End.py
+++ DoDiligence/Back_End.py
@@ -12,10 +12,15 @@ hist_div_payout_dates = hist_data[hist_data.Dividends != 0]  # filters old to re
 hist_div_data = hist_div_payout_dates['Dividends']
 
 grp_hist_div_payout_dates = hist_div_data.resample('A').sum()
-print(grp_hist_div_payout_dates)
+#print(grp_hist_div_payout_dates)
+
+div_yield = 100*hist_div_data/hist_data['Close']
+div_yield = div_yield.dropna()
+grp_div_yield = div_yield.resample('A').mean()
+print(grp_div_yield)
 
 # #subplot creation
-fig, (close, dividends) = plt.subplots(nrows=2, ncols=1)
+fig, (close, dividends, dividend_yield) = plt.subplots(nrows=3, ncols=1)
 
 # Historical cLose price plot
 y = hist_data['Close']
@@ -26,7 +31,9 @@ close.plot(x, y, linewidth=1.0)
 y_div = grp_hist_div_payout_dates.iloc[:, ]
 x_div = grp_hist_div_payout_dates.index
 dividends.bar(x_div, y_div,100)
+
+#Dividend yield plot
+y_ydiv = grp_div_yield.iloc[:,]
+x_ydiv = grp_div_yield.index
+dividend_yield.bar(x_ydiv, y_ydiv, 100)
 plt.show()
-
-
-
